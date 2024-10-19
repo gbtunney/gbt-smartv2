5c5f07f03bb77113ed37c322d33bd7497f5445b8
Gillian Tunney <gbtunney@mac.com>
2024-10-19 19:31:46 -0400

    fix(node-red-runtime,node-red-contrib): some dumb crap

--- packages/node-red-contrib/src/nodes/lower-case/index.ts
+++ packages/node-red-contrib/src/nodes/lower-case/index.ts
@@ -12,9 +12,6 @@ export type TransformTextOptions = {
 export type TransformTextNodeDef = {} & NodeDef & TransformTextOptions
 export type TransformTextNode = Node
 
-//import { TransformTextNode, TransformTextNodeDef } from './modules/types.js'
-//import { TRANSFORM_TEXT_OPERATION } from './shared/types.js'
-
 /* eslint @typescript-eslint/naming-convention  : "warn" */
 const nodeInit: NodeInitializer = (RED): void => {
     function LowerCaseNode(
@@ -32,50 +29,9 @@ const nodeInit: NodeInitializer = (RED): void => {
             send(msg)
             done()
         })
-        //_node.on('input',(msg:{payload:string})=>{
-        //  msg.payload = msg.payload.toLowerCase();
-        //_node.send(msg)
-        //  })
-        /*   node.on('input', function(msg:{payload:string},send,done){
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });*/
         RED.nodes.registerType('lower-case', LowerCaseNode)
     }
-    /*  function TransformTextNodeConstructor(
-              this: TransformTextNode,
-              config: TransformTextNodeDef,
-          ): void {
-              RED.nodes.createNode(this, config)
-
-              switch (config.operation) {
-                  case TRANSFORM_TEXT_OPERATION.UpperCase: {
-                      this.on('input', (msg, send, done) => {
-                          if (typeof msg.payload === 'string') {
-                              msg.payload = msg.payload.toUpperCase()
-                          }
-                          send(msg)
-                          done()
-                      })
-                      break
-                  }
-                  case TRANSFORM_TEXT_OPERATION.LowerCase: {
-                      this.on('input', (msg, send, done) => {
-                          if (typeof msg.payload === 'string') {
-                              msg.payload = msg.payload.toLowerCase()
-                          }
-                          send(msg)
-                          done()
-                      })
-                      break
-                  }
-              }
-          }*/
-
-    //  RED.nodes.registerType('lowercase-text', funcLowercase)
-    console.log('THE NODE INIT!!!!!!!!!!', RED.nodes)
+    console.log('CUSTOM NODE INITIALIZED', RED.nodes)
 }
-
-console.log('THE NODE INIT!!!!!!!!!!plain')
 
 export default nodeInit
