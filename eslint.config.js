628881f0eaffa57bb74a9bf5db9a99704c65057c
Gillian Tunney <gbtunney@mac.com>
2024-10-31 18:21:46 -0400

    feat(root): added markdown lint

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default [
         rules: {
             '@typescript-eslint/no-this-alias': 'off',
             'import/no-default-export': 'off',
+            'jsdoc/check-indentation': 'off',
         },
     }),
 ]
